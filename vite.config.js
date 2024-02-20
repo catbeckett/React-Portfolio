a3dfb33c2a3b79662bbc7f23bdcafb7aff869dc8
Cat <catejb89@gmail.com>
2024-02-20 09:21:46 +0000

    Update vite.config.js configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//https://catbeckett.github.io/React-Portfolio/
 export default defineConfig({
   plugins: [react()],
 })
