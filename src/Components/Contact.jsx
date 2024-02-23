62fff0397d3df2928a9e39a2899bcc6640f91a0e
Cat <catejb89@gmail.com>
2024-02-23 19:30:17 +0000

    Resolved conflicts

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
 import './ContactForm.css';
 
 function Contact() {
@@ -9,9 +7,16 @@ function Contact() {
 
   const onSubmit = async (data) => {
     try {
-      const response = await axios.post('/api/contact', data); // Updated endpoint URL
+      // Send form data to Formspree endpoint
+      const response = await fetch('https://formspree.io/f/mnqevnzp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
 
-      if (response.status === 200) {
+      if (response.ok) {
         console.log('Form submitted successfully!');
       } else {
         console.error('Failed to submit form:', response.statusText);
@@ -30,7 +35,7 @@ function Contact() {
             <li><a href="https://github.com/catbeckett">GitHub Profile</a></li>
             <li><a href="https://www.linkedin.com/in/catherinebeckett11">LinkedIn Page</a></li>
             <li>Email: catejb89@gmail.com</li>
-            <li><Link to="https://drive.google.com/file/d/1U3m45sVzXdq--fsc7Q0hcDZH4u5fFWow/view?usp=drive_link">PDF version of my resume</Link></li>
+            <li><a href="https://drive.google.com/file/d/1U3m45sVzXdq--fsc7Q0hcDZH4u5fFWow/view?usp=drive_link">PDF version of my resume</a></li>
           </ul>
         </div>
         <div className="contactForm">
